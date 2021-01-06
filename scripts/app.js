55687abd85f05f84eb3047507a769007c65e5b39
Luan Dias <luandias1312@gmail.com>
2021-01-06 15:49:00 -0300

    correction bug and finalization project

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,34 @@
 const date = new Date()
 const dates = new Array('domingo','segunda-feira','terça-feira','quarta-feira','quinta-feira','sexta','sábado')
 const months = new Array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro")
+const spanClose = document.getElementById('close')
+const openModal = document.getElementById('open-modal')
+let modal = document.getElementById('modal')
+let listTasks = document.getElementById('tasks')
+
 
 document.getElementById("day").innerHTML = date.getDate() > 9 ? date.getDate() : '0' + date.getDate()
 document.getElementById("month").innerHTML = months[date.getMonth()]
 document.getElementById("year").innerHTML = date.getFullYear()
 document.getElementById("day-week").innerHTML = dates[date.getDay()]
 
-console.log(date.getDate())
-console.log(date.getDay())
-console.log(date.getFullYear())
+let tasks = []
 
-const tasks = []
+spanClose.addEventListener("click", () => {
+    modal.classList.remove('active')
+})
+
+openModal.addEventListener('click', () => {
+    modal.classList.add('active')
+})
 
 const idGenerate = () => {
     return Math.floor(Math.random() * (100 - 1) + 1)
 }
 
 function newTask() {
+    titleTask = document.getElementById('titleTask').value
+    taskDescription = document.getElementById('descriptionTask').value
     let task = {
         id: idGenerate(),
         data: {
@@ -26,39 +37,44 @@ function newTask() {
         }
     }
     tasks.push(task)
+    modal.classList.remove('active')
+    titleTask = document.getElementById('titleTask').value = ''
+    taskDescription = document.getElementById('descriptionTask').value = ''
     updateScreen()
 }
 
 const updateScreen = () => {
-    let listTasks = document.getElementById('#tasks')
+    
+    listTasks.innerHTML = ""
     tasks.forEach(item => {
         let task = document.createElement('div')
-        let taskTitle = document.creatElement('h3')
-        let taskDescription = document.creatElement('div')
-        let deleteTask = document.creatElement('span')
+        let taskTitle = document.createElement('h3')
+        let taskDescription = document.createElement('div')
+        let taskDelete = document.createElement('button')
         let textTitleTask = document.createTextNode(item.data.title)
         let textDescriptionTask = document.createTextNode(item.data.description)
     
-        task.setAttribute('id-data', task.id)
+        task.setAttribute('id-data', item.id)
         task.setAttribute('class', 'task')
         taskTitle.setAttribute('class', 'task-title')
         taskDescription.setAttribute('class', 'task-description')
-        deleteTask.setAttribute('id-data', item.id)
-        deleteTask.setAttribute('class', 'close')
-        deleteTask.setAttribute('onclick', 'deleteTask(this)')
-        deleteTask.innerHTML = 'x'
+        taskDelete.setAttribute('id-data', item.id)
+        taskDelete.setAttribute('class', 'close')
+        taskDelete.setAttribute('onclick', 'deleteTask(this)')
+        taskDelete.innerHTML = 'x'
 
-        taskTitle.innerHTML = textTitleTask
-        taskDescription.innerHTML = textDescriptionTask
+        taskTitle.appendChild(textTitleTask)
+        taskDescription.appendChild(textDescriptionTask)
 
         task.appendChild(taskTitle)
         task.appendChild(taskDescription)
-        task.appendChild(deleteTask)
+        task.appendChild(taskDelete)
         listTasks.appendChild(task)
     })
 }
 
 function deleteTask(element) {
-    tasks.filter(task => task.id != element.getAttribute('id-data'))
+    console.log(element.getAttribute('id-data'))
+    tasks = tasks.filter(task => task.id != element.getAttribute('id-data'))
     updateScreen()
 }
